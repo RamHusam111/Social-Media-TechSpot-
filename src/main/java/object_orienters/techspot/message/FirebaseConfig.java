e399f1c24ace68e62f7fa2c6f48660fedcec878e
RamHusam111 <husamramoni@gmail.com>
2024-04-22 21:00:13 +0300

    changes private firebase api key

--- src/main/java/object_orienters/techspot/message/FirebaseConfig.java
+++ src/main/java/object_orienters/techspot/message/FirebaseConfig.java
@@ -22,9 +22,8 @@ public class FirebaseConfig {
     @PostConstruct
     public void initializeFirebase() throws IOException {
         logger.info("Initializing Firebase");
-        FileInputStream serviceAccount = new FileInputStream(
-                "src/main/resources/techspot-objectorienters-firebase-adminsdk-bqho2-92fdd9f633.json");
-
+        FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/techspot-objectorienters-firebase-adminsdk-bqho2-f5981f1058.json");
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
